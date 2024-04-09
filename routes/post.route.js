92080b571a688373494bfa00353e2a78bce2e7a3
Alek30k <alemank23k@gmail.com>
2024-04-09 11:21:53 -0300

    last commit

---
+++ routes/post.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+const router = express.Router();
+
+// router.get("/ale", (req, res) => {
+//   console.log("router works");
+// });
+
+// router.post("/ale", (req, res) => {
+//   console.log("router works");
+// });
+
+// router.put("/ale", (req, res) => {
+//   console.log("router works");
+// });
+
+// router.delete("/ale", (req, res) => {
+//   console.log("router works");
+// });
+
+export default router;
