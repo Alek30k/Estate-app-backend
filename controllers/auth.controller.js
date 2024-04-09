1f996f33d38674c9bc486643205ff957a0f13e7f
Alek30k <alemank23k@gmail.com>
2024-04-09 16:03:50 -0300

    last commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,8 @@
 export const register = (req, res) => {
+  const { username, email, password } = req.body;
   console.log(req.body);
 };
+
 export const login = (req, res) => {};
+
 export const logout = (req, res) => {};
