a3165c9c9c4060485dff150af0bed4607aa394a5
Alek30k <alemank23k@gmail.com>
2024-04-13 15:09:49 -0300

    last commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,3 +9,32 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ message: "Failed to get users!" });
   }
 };
+
+export const getUser = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get users!" });
+  }
+};
+
+export const updateUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to update users!" });
+  }
+};
+export const deleteUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to delete users!" });
+  }
+};
