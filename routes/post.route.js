a5a5b14d0b97ff7bd6f3f20e3314fda6932ee153
Alek30k <alemank23k@gmail.com>
2024-04-14 14:01:03 -0300

    last commit

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../controllers/post.controller.js";
+
 const router = express.Router();
 
-router.get("/posta", createPost);
-
-// router.post("/ale");
-
-// router.put("/ale");
-
-// router.delete("/ale");
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken, addPost);
+router.put("/:id", verifyToken, updatePost);
+router.delete("/:id", verifyToken, deletePost);
 
 export default router;
