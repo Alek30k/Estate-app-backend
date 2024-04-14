70450d5bae3619a66bebc2aa7868941896470c54
Alek30k <alemank23k@gmail.com>
2024-04-14 13:59:50 -0300

    last commit

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,5 +1,28 @@
-export const createPost = (req, res) => {
-  console.log("post");
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+
+export const getPosts = async (req, res) => {
+  const query = req.query;
+
+  try {
+    const posts = await prisma.post.findMany({
+      where: {
+        city: query.city || undefined,
+        type: query.type || undefined,
+        property: query.property || undefined,
+        bedroom: parseInt(query.bedroom) || undefined,
+        price: {
+          gte: parseInt(query.minPrice) || undefined,
+          lte: parseInt(query.maxPrice) || undefined,
+        },
+      },
+    });
+
+    // setTimeout(() => {
+    res.status(200).json(posts);
+    // }, 3000);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get posts" });
+  }
 };
-// export const login = (req, res) => {};
-// export const logout = (req, res) => {};
