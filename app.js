a8a9bae92773e6db36229ce6ab81155bfa3f6eb9
Alek30k <alemank23k@gmail.com>
2024-04-17 14:59:26 -0300

    last commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use("/api/users", usersRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
-app.use("/api/message", messageRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
   console.log("Server is running!");
