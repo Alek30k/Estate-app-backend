25f9704227a49f64f4cc1745337f40689cb0e6bc
Alek30k <alemank23k@gmail.com>
2024-04-19 10:20:33 -0300

    last commit

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -5,10 +5,14 @@ export const verifyToken = (req, res, next) => {
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "Token is not Valid!" });
-    req.userId = payload.id;
+  jwt.verify(
+    token,
+    "Q3k5EuDm3EnrVFbBX5ciZ4fQ8oV2sdBGNtH7NhP2Zzc=",
+    async (err, payload) => {
+      if (err) return res.status(403).json({ message: "Token is not Valid!" });
+      req.userId = payload.id;
 
-    next();
-  });
+      next();
+    }
+  );
 };
