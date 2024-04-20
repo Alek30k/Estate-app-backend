92972a447f8c7a6758b7e8526bd743b59342d893
Alek30k <alemank23k@gmail.com>
2024-04-20 13:18:23 -0300

    last commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -13,7 +13,7 @@ import {
 const router = express.Router();
 
 router.get("/", getUsers);
-router.get("/search/:id", verifyToken, getUser);
+// router.get("/search/:id", verifyToken, getUser);
 // router.put("/:id", verifyToken, updateUser);
 // router.delete("/:id", verifyToken, deleteUser);
 // router.post("/save", verifyToken, savePost);
