0542c87011c758d59d5f1d50b13b1bb0b7e891cc
Alek30k <alemank23k@gmail.com>
2024-04-23 14:07:10 -0300

    last commit

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies?.token;
+  const token = req.cookies.token;
 
   // console.log(token);
 
-  // if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
   jwt.verify(token, "Q3k5EuDm3EnrVFb", async (err, payload) => {
-    // if (err) {
-    //   console.error("Error verifying token:", err);
-    //   return res.status(403).json({ message: "Token is not Valid!" });
-    // }
+    if (err) {
+      console.error("Error verifying token:", err);
+      return res.status(403).json({ message: "Token is not Valid!" });
+    }
     req.userId = payload.id;
 
     next();
