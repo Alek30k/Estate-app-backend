7222d773e324844483dc7fece95003e772b29e8d
Alek30k <alemank23k@gmail.com>
2024-04-23 12:14:14 -0300

    last commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -14,15 +14,15 @@ const router = express.Router();
 
 router.get("/", getUsers);
 // router.get("/search/:id", verifyToken, getUser);
-router.put("/:id", verifyToken, updateUser);
-router.delete("/:id", verifyToken, deleteUser);
-router.post("/save", verifyToken, savePost);
-router.get("/profilePosts", verifyToken, profilePosts);
-router.get("/notification", verifyToken, getNotificationNumber);
 // router.put("/:id", verifyToken, updateUser);
-// router.delete("/:id", deleteUser);
-// router.post("/save", savePost);
-// router.get("/profilePosts", profilePosts);
-// router.get("/notification", getNotificationNumber);
+// router.delete("/:id", verifyToken, deleteUser);
+// router.post("/save", verifyToken, savePost);
+// router.get("/profilePosts", verifyToken, profilePosts);
+// router.get("/notification", verifyToken, getNotificationNumber);
+router.put("/:id", verifyToken, updateUser);
+router.delete("/:id", deleteUser);
+router.post("/save", savePost);
+router.get("/profilePosts", profilePosts);
+router.get("/notification", getNotificationNumber);
 
 export default router;
