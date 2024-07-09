3cdc3e8fbe09ac85d314b7d7de7396b6b65450eb
Alek30k <alemank23k@gmail.com>
2024-07-09 13:58:54 -0300

    last commit

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://estate-app-beta.vercel.app", credentials: true })
+);
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
